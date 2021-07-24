00a0e9819185ed2322f41d522e0bc036c5d4509f
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-24 18:34:46 +0900

    wordcloud追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <vue-word-cloud
+      style="height: 480px; width: 640px;"
+      :words="[
+        ['romance', 19],
+        ['horror', 3],
+        ['fantasy', 7],
+        ['adventure', 3],
+        ['romance', 9],
+        ['horror', 33],
+        ['fantasy', 72],
+        ['adventure', 13],
+      ]"
+      :color="([, weight]) => (weight > 10 ? 'DeepPink' : weight > 5 ? 'RoyalBlue' : 'Indigo')"
+      font-family="Roboto"
+    />
+  </div>
 </template>
 
 <script>
+import VueWordCloud from "vuewordcloud";
+
 export default {
   name: "App",
-  components: {},
+  components: { [VueWordCloud.name]: VueWordCloud },
 };
 </script>
 
