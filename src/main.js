8c32638f2170f03184c4200245e04e52b84b45e7
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-28 12:40:25 +0900

    test

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueAnalytics, {
-  id: "G-1TNZ7QX62E",
-  router,
-});
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import VueAnalytics from "vue-analytics";
 
 import WordCloud from "../components/WordCloud.vue";
 
 Vue.use(VueRouter);
+
+Vue.use(VueAnalytics, {
+  id: "G-1TNZ7QX62E",
+});
 
 export default new VueRouter({
   mode: "history",
