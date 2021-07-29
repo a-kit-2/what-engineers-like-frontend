695db916dc28d9bd4252c56771bc6049bbac577e
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-29 13:41:46 +0900

    test

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueAnalytics from "vue-analytics";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, { property: { id: "G-KKWC3Z231X", router } });
 
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
-
-Vue.use(VueAnalytics, {
-  property: {
-    id: "G-KKWC3Z231X",
-    router,
-  },
-});
