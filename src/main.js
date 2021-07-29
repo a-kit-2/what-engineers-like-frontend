6ef6aa28d3518936116fcf62d827e9781071beda
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-29 13:29:02 +0900

    test

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueAnalytics, {
-  id: "G-KKWC3Z231X",
-});
-
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+Vue.use(VueAnalytics, {
+  id: "G-KKWC3Z231X",
+});
